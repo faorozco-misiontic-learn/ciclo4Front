05e448ec905d18770a0f8f41468b5025284bd63b
faorozco <faorozco@utp.edu.co>
2021-11-30 21:42:57 -0500

    cambio login a home

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import PrivateRoute from '../auth/privateroute';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "../auth/privateroute";
 
-import login from '../login/login';
-import CrudSimple from '../crudSimple';
-import Home from '../pages/home/home';
+import login from "../login/login";
+import CrudSimple from "../crudSimple";
+import Home from "../pages/home/home";
 
 export default function AppRouter() {
   return (
     <Router>
       <Switch>
-        <Route exact path={['/']} component={Home} />
-        <Route exact path={['/login']} component={login} />
+        <Route exact path={["/login"]} component={Home} />
+        <Route exact path={["/login"]} component={login} />
         <PrivateRoute exact path="/crudSimple" component={CrudSimple} />
         <Route
-          path={'*'}
+          path={"*"}
           component={() => (
             <h1 style={{ marginTop: 300 }}>
               404
